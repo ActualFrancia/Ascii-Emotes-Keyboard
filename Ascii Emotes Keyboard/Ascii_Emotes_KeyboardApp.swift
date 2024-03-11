1172bf3f8f6537a116957dc683c8c4b60ae40b5c
Francia <141374480+ActualFrancia@users.noreply.github.com>
2024-03-11 18:22:54 -0400

    Initial Commit

---
+++ Ascii Emotes Keyboard/Ascii_Emotes_KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ascii_Emotes_KeyboardApp.swift
+//  Ascii Emotes Keyboard
+//
+//  Created by Kali Francia on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Ascii_Emotes_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
